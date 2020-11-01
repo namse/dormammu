aad4f7dba6effbc88d4eb95ad5dc21f4dec48b3e
skatpgusskat <skatpgusskat@naver.com>
2020-11-01 22:30:31 +0900

    v1.0

--- main.js
+++ main.js
@@ -1,19 +1,106 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, globalShortcut, ipcMain, dialog, screen } = require('electron')
+const prompt = require('electron-prompt');
+const Store = require('electron-store');
 
-function createWindow () {
+const store = new Store();
+
+function createWindow() {
   // Create the browser window.
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width,
+    height,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      nodeIntegration: true,
+    },
+    frame: false,
+    transparent: true,
+    skipTaskbar: true,
   })
+  mainWindow.setIgnoreMouseEvents(true);
+  mainWindow.setAlwaysOnTop(true);
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+
+  let volumePercent = store.get('volumePercent');
+  if (!volumePercent && volumePercent !== 0) {
+    volumePercent = 75;
+  }
+  mainWindow.webContents.once('dom-ready', () => {
+    mainWindow.webContents.send('setVolume', volumePercent / 100);
+  });
+
+  app.whenReady().then(() => {
+    globalShortcut.register('Shift+F2', () => {
+      console.log("shift f2");
+      mainWindow.webContents.send('play');
+    })
+
+    globalShortcut.register('Shift+F3', () => {
+      console.log("shift f3");
+      mainWindow.webContents.send('stop');
+    })
+
+    globalShortcut.register('Shift+F7', async () => {
+      try {
+        console.log("shift f7");
+        const nextVolumeString = await prompt({
+          title: '볼륨 설정',
+          label: `현재 볼륨은 ${volumePercent}% 입니다. 새 볼륨 0 ~ 100 사이의 값을 입력해주세요.`,
+          value: volumePercent,
+          type: 'input',
+          width: 700,
+        });
+        const parsed = parseInt(nextVolumeString);
+        if (isNaN(parsed)) {
+          dialog.showMessageBox({
+            title: '볼륨 설정',
+            message: `${nextVolumeString}은 잘못된 값입니다. 0이상 100이하의 숫자를 입력해주세요.`
+          })
+          return;
+        }
+
+        const {
+          response
+        } = await dialog.showMessageBox({
+          title: '볼륨 설정',
+          message: `볼륨을 ${parsed}%로 설정합니까?`,
+          buttons: ['네', '아니오'],
+        });
+
+        const isYes = response === 0;
+
+        if (!isYes) {
+          dialog.showMessageBox({
+            title: '볼륨 설정',
+            message: '취소하였습니다.'
+          })
+          return;
+        }
+
+        dialog.showMessageBox({
+          title: '볼륨 설정',
+          message: `볼륨이 ${parsed}%로 설정되었습니다.`,
+        })
+
+        volumePercent = parsed;
+        store.set('volumePercent', volumePercent);
+        mainWindow.webContents.send('setVolume', volumePercent / 100);
+      } finally {
+        mainWindow.setAlwaysOnTop(true);
+      }
+    })
+
+    globalShortcut.register('Shift+F8', async () => {
+      await dialog.showMessageBox({
+        title: '종료합니다.',
+        message: '빠이빠이~'
+      })
+      app.exit();
+    })
+  })
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -24,13 +111,14 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
--- renderer.js
+++ renderer.js
@@ -1,6 +1,45 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const { ipcRenderer } = require("electron")
+
+window.onload = () => {
+  let volume = 0;
+  const video = document.createElement('video');
+  let isPaused = true;
+  video.src = "dormammu.mp4";
+  video.onpause = () => {
+    isPaused = true;
+  }
+
+  const tick = () => {
+    if (isPaused) {
+      if (!video.paused) {
+        video.pause();
+      }
+      if (video.parentElement) {
+        video.parentElement.removeChild(video);
+      }
+    }
+    requestAnimationFrame(tick)
+  };
+  tick();
+
+  ipcRenderer.on('play', () => {
+    console.log('play');
+    console.log('volume', volume);
+    isPaused = false;
+    if (!video.parentElement) {
+      document.body.appendChild(video);
+    }
+    video.volume = volume;
+    video.currentTime = 0;
+    video.play();
+  });
+
+  ipcRenderer.on('stop', () => {
+    isPaused = true;
+  });
+
+  ipcRenderer.on('setVolume', (event, nextVolume) => {
+    volume = nextVolume;
+    console.log(volume);
+  });
+}
